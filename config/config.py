c47e069a3fd291fe9a89cafa8463075fc3f50f10
voinkovegor <voinkov-egor@mail.ru>
2023-08-26 09:38:50 +0500

    Little refactor

--- config/config.py
+++ config/config.py
@@ -6,6 +6,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str = "mongodb://host.docker.internal:27017"  # Подключится в докере без .dev.env
 
+    class Config:
+        env_file = ".local.env"
 
-prefix = os.getenv("ENV_STATE", "local")
-settings = Settings(_env_file=f'.{prefix}.env', _env_file_encoding='utf-8')
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from fastapi import FastAPI
+import uvicorn
 
-from database import close_db_connection
-from parser.router import router as router_parser
-from api.router import router as router_api
-
-
-app = FastAPI()
-app.include_router(router_parser)
-app.include_router(router_api)
-app.add_event_handler("shutdown", close_db_connection)
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, reload=True)
