f495191fb5b2258c2fd97f10d4228a6e91971d54
voinkovegor <voinkov-egor@mail.ru>
2023-08-27 10:16:34 +0500

    Setup unittests

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config.config import settings
 
 async_client = AsyncIOMotorClient(settings.DATABASE_URL)
-db = async_client.get_database("parser_db")
+db = async_client.get_database(settings.DB_NAME)
 htmls = db.get_collection("htmls")
 # Создал индекс по уникальному полю url
 # В рамках тестового без паролей
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -6,21 +6,28 @@ from mongomock_motor import AsyncMongoMockClient
 from starlette.testclient import TestClient
 
 from app import app
+from config.config import settings
+from database import db
 from tests.mock_data import FIRST_DOCUMENT
 from tests.mock_functions import mock_parser
 
 
 class TestParser(unittest.IsolatedAsyncioTestCase):
+    client = AsyncMongoMockClient()
+    db = client.get_database(settings.DB_NAME)
+    app = TestClient(app)
+
     async def asyncSetUp(self):
-        self.client = AsyncMongoMockClient()
-        self.db = self.client.get_database("test_parser_db")
-        self.app = TestClient(app)
+        pass
 
     async def asyncTearDown(self):
-        await self.db.htmls.drop()
-        self.client.close()
+        await db.htmls.drop()
 
     async def test_parser_default_params(self):
+        """
+        Проверка работы парсера с мок-данными о сайте, сохранением в тестовую базу
+        :return:
+        """
         patch_parser = mock.patch(
             "parser.parser_logic.parser",
             mock_parser,
@@ -32,12 +39,8 @@ class TestParser(unittest.IsolatedAsyncioTestCase):
 
         patch_parser.stop()
 
-        # await self.db.htmls.insert_one({"9": 2, "3": 4})
-        # print(await self.db.htmls.find().to_list(length=2))
-        # print(await self.db.get_collection("htmls").find().to_list(length=2))
-        # print(await htmls.count_documents({}))
         TestCase().assertEqual(result.status_code, 201)
-        list_documents = await self.db.htmls.find().to_list(length=None)
+        list_documents = await db.htmls.find().to_list(length=None)
         cleaned_docs = [{"url": item["url"], "title": item["title"], "html": item["html"]} for item in list_documents]
         TestCase().assertEqual(cleaned_docs, [FIRST_DOCUMENT])
 
